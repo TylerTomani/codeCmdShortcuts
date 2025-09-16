68bb3f13d22e930255e788bcb92141f6d2ec351a
tt <tt@MacBookPro.lan>
2025-09-16 05:18:54 -0400

    main

--- js/ui/drop-downs.js
+++ js/ui/drop-downs.js
@@ -1,8 +1,8 @@
 // drop-downs.js
 function hideAllDropChildren(dropChilds) {
     dropChilds.forEach(el => {
-        // console.log(el)
-        // return    
+        console.log(el)
+        el.classList.add('hide')
     })
 }
 export function initDropDowns() {   
@@ -14,36 +14,46 @@ export function initDropDowns() {
     }
     // const dropChilds = document.querySelectorAll('.code-cmd') ? document.querySelectorAll('.code-cmd') : document.querySelectorAll('.topic-snips')
     const dropDownChildren =  document.querySelectorAll('.topic-snips,.code-cmd')
+    hideAllDropChildren(dropDownChildren)
     function handleToggle(e) {
         let target;        
-        hideAllDropChildren(dropDownChildren)
         if (e.type === "keydown") {
-            if ((e.key === "Enter" || e.key === " ") && document.activeElement.classList.contains("drop-down")) {
+            if ((e.key === "Enter" || e.key === " ") && (document.activeElement.classList.contains("drop-down") || document.activeElement.classList.contains("drop-code-cmd"))) {
                 e.preventDefault();
                 target = document.activeElement;
-                console.log(target)
             } else {
                 return; // ignore other keys
             }
         } else if (e.type === "click") {
             // Ignore clicks triggered by keyboard
             if (e.detail === 0) return;
-            target = e.target.closest(".drop-down");
+            target = e.target.closest(".drop-down") || e.target.closest(".drop-code-cmd");
             if (!target) return;
         }
 
         // Unified toggle logic
         const topic = target.closest(".topic");
+        const snip = target.closest(".snip");
         // console.log("Toggled dropdown:", topic);
+        // if (!topic || !snip) return;
+        console.log(snip)
+        if (snip){
+            toggleCodeSnips(snip)
+            return
+        } 
         if(topic){
             toggleTopicSnips(topic)
-        }
-        if (!topic) return;
+            return 
+        } 
     }
 }
 
+function toggleCodeSnips(snip) {
+    const codeCmd = snip.querySelector('.code-cmd')
+    codeCmd.classList.toggle('hide')
+
+}
 function toggleTopicSnips(topic) {
-    const topicSnips = topic.querySelector('.topic-snips,.code-cmd')
-    // topicSnips.classList.toggle("collapsed"); // example toggle
+    const topicSnips = topic.querySelector('.topic-snips')
     topicSnips.classList.toggle("hide"); // example toggle
 }
